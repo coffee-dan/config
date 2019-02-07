c3bbce986dfcf1edcd1983d106285f7a639be24d
ubuntu-dan <coffee-dan@protonmail.com>
2019-02-06 21:46:17 -0600

    Full update of dan startpage

--- dan/weather.js
+++ dan/weather.js
@@ -1,8 +1,10 @@
 window.onload = () => {
 	let xhr = new XMLHttpRequest();
 	// Request to open weather map
-	// Arlington - http://api.openweathermap.org/data/2.5/weather?id=4671240&units=imperial&appid=bb617c8eebae0d1c79c479dd6ad0aab3
-	xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?id=4691930&units=imperial&appid=bb617c8eebae0d1c79c479dd6ad0aab3');
+	// Arlington - id=4671240
+	// Fort Worth - id=4691930
+	// Burleson - id=4677551
+	xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?id=4671240&units=imperial&appid=bb617c8eebae0d1c79c479dd6ad0aab3');
 	xhr.onload = () => {
 		if (xhr.readyState === 4) {
 			if (xhr.status === 200) {
@@ -10,6 +12,8 @@ window.onload = () => {
 				console.log(json);
 				document.getElementById("temp").innerHTML = json.main.temp.toFixed(0) + "°F";
 				document.getElementById("weather-description").innerHTML = json.weather[0].description;
+				document.getElementById("humidity").innerHTML = json.main.humidity.toFixed(2) + "%";
+				document.getElementById("name").innerHTML = json.name;
 			} else {
 				console.log('error msg: ' + xhr.status);
 			}
